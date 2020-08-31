b066325cadfca3d0cef05f84548d76ca14cd6f60
Flavio Oliveira <fgo.flavio@gmail.com>
2020-08-31 20:41:59 -0300

    Adicionado melhoria rota de put

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -51,5 +51,17 @@ app.patch('/student/:id', async (req, res) => {
     res.status(500).send(err);
   }
 });
+app.put('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findOneAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
 
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 export { app as studentRouter };
